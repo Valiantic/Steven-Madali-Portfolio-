86b5561236b5f067779152920479f51336173df0
StevenMadali <stevenmadali17@gmail.com>
2024-10-14 22:49:19 +0800

    Add Profile Picture

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,15 @@ const text = document.querySelector(".headText")
 
 const writeText = () => {
     setTimeout(() => {
-        text.textContent = "Software Engineer";
+        text.textContent = "Software Engineer!";
         text.style.color = "rgb(48, 196, 255)";
     }, 0);
     setTimeout(() => {
-        text.textContent = "Web Developer";
+        text.textContent = "Web Developer!";
         text.style.color = "rgb(255, 72, 0)";
     }, 4000);
     setTimeout(() => {
-        text.textContent = "Web Designer";
+        text.textContent = "Web Designer!";
         text.style.color = "rgb(198, 42, 255)";
     }, 8000);
 }
